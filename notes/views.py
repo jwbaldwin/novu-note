dc84b5a139bcdbc664b161bdb08a072db5a765fc
jbaldwin10 <jwbaldwin3@aol.com>
2018-05-15 17:03:16 -0400

    Adding tests for views and models

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Note
 
@@ -12,11 +13,18 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_notes_list'
 
     def get_queryset(self):
-        """Return all published notes."""
-        return Note.objects.order_by('-pub_date')
+        """
+        Return the last published notes (not including those set to be published in the future).
+        """
+        return Note.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
     """Generic detail view"""
     model = Note
     template_name = 'notes/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any notes that aren't published yet.
+        """
+        return Note.objects.filter(pub_date__lte=timezone.now())
