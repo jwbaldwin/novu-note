038d31d94f1214f6d70c5830442d4a44e3aa49ad
jbaldwin10 <jwbaldwin3@aol.com>
2018-05-23 10:07:39 -0400

    adding detail to note and permissions

--- api/models.py
+++ api/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Note(models.Model):
     """This class represents the note model."""
     text = models.CharField(max_length=255, blank=False, unique=False)
+    category_tags = ArrayField(models.CharField(max_length=20, blank=True, null=True))
+
+    creator = models.ForeignKey('auth.User',
+    related_name='notes', 
+    on_delete=models.CASCADE) 
+
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from .permissions import IsCreator
 from .serializers import NoteSerializer
 from .models import Note
 
@@ -6,12 +7,14 @@ class CreateView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = ( permissions.IsAuthenticated, IsCreator )
 
     def perform_create(self, serializer):
         """Save the post data when creating a new note."""
-        serializer.save()
+        serializer.save(creator=self.request.user)
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    permission_classes = ( permissions.IsAuthenticated, IsCreator )
