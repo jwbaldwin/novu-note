e91e350ece14ab7f99ed945051fb57d791e691ec
jbaldwin10 <jwbaldwin3@aol.com>
2018-05-29 15:44:23 -0400

    trying only one html template

--- neuro/settings.py
+++ neuro/settings.py
@@ -70,7 +70,9 @@ ROOT_URLCONF = 'neuro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+                    os.path.join(BASE_DIR, './frontend/public')
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
