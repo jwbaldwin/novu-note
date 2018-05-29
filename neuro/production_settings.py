9b76de7ced2d33a8503a45f7a384b85e14779689
jbaldwin10 <jwbaldwin3@aol.com>
2018-05-29 17:27:21 -0400

    cleaning up

--- neuro/production_settings.py
+++ neuro/production_settings.py
@@ -1,12 +1,12 @@
 from .settings import *
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "build"),
 ]
 
 WEBPACK_LOADER = {
     'DEFAULT': {
             'BUNDLE_DIR_NAME': 'bundles/',
-            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.prod.json'),
+            'STATS_FILE': os.path.join(BASE_DIR+'/stats', 'webpack-stats.prod.json'),
         }
 }
--- neuro/settings.py
+++ neuro/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
 WEBPACK_LOADER = {
     'DEFAULT': {
             'BUNDLE_DIR_NAME': 'bundles/',
-            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+            'STATS_FILE': os.path.join(BASE_DIR+'/stats', 'webpack-stats.dev.json'),
         }
 }
 
