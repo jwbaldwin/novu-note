fd6859dde58323950ef850e4dc4d4e5329b6e86e
jbaldwin10 <jwbaldwin3@aol.com>
2018-05-30 11:23:54 -0400

    serving staticfiles with whitenoise

--- neuro/production_settings.py
+++ neuro/production_settings.py
@@ -6,6 +6,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "build"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 WEBPACK_LOADER = {
     'DEFAULT': {
             'BUNDLE_DIR_NAME': 'bundles/',
