15081040eb74cf51dc9b60247c4cf6dbafe618d9
jbaldwin10 <jwbaldwin3@aol.com>
2018-06-01 13:25:03 -0400

    login page frontend work

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Sidebar from './sidebar/Sidebar';
 import Navbar from './navbar/Navbar';
+import ColorLogo from '../../images/color-logo@3x.png';
 
 class Header extends Component {
     render() {
         return (
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-                    <a className="navbar-brand" >NovuNote</a>
+                    <a className="navbar-brand">
+                        <img src={ColorLogo} alt="logo" width="12%" height="10%" />
+                    </a>
                     <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ColorLogo from '../../images/color-logo@3x.png';
 import './Login.css';
 
 export default class Login extends Component {
@@ -6,6 +7,9 @@ export default class Login extends Component {
         return (
             <div className="container">
                 <form className="form-signin" onSubmit={this.handleSubmit}>
+                    <div className="logo">
+                        <img src={ColorLogo} alt="logo" />
+                    </div>
                     <h1 className="form-signin-heading">NovuNote</h1>
                     <input type="text" className="form-control" name="username" placeholder="Email Address" required="" autoComplete="username" autoFocus="" ref="email" />
                     <input type="password" className="form-control" name="password" placeholder="Password" required="" autoComplete="current-password" ref="password" />
