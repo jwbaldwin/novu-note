6495b65fd7fd813885605a4c78753814a2f743ee
jbaldwin10 <jwbaldwin3@aol.com>
2018-06-04 17:48:13 -0400

    add_note works, key was realizing return state and state.xyz.concat(object)

--- frontend/src/reducers/noteReducer.jsx
+++ frontend/src/reducers/noteReducer.jsx
@@ -14,7 +14,7 @@ export default function (state = initialState, action) {
         case ADD_NOTE:
             return { 
                 ...state,
-                items: [...state.arr, action.payload]
+                items: [ ...state.items, action.payload ]
             };
         default:
             return state;
